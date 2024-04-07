d895b599901eb606c3f6643f33026e66f9038db4
kamila <230834@astanait.edu.kz>
2024-04-07 22:08:31 +0500

    3 task

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int n = 5;
         int[] arr = new int[]{4, 2, 6, 3, 1};
-       // System.out.println(minElement(n, arr));
-        System.out.println(average(n, arr));
+        // System.out.println(minElement(n, arr));
+        // System.out.println(average(n, arr));
+        System.out.println(isPrime(7));
 
     }
 
